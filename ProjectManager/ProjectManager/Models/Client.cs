35a996f7833f632e24f585dbd6ed17ce8fecacbd
jmattefs <josephmattefs@gmail.com>
2016-03-08 15:39:07 -0600

    first

---
+++ ProjectManager/ProjectManager/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public int Role { get; set; }
+        public string Name { get; set; }
+        public Project Project { get; set; }
+        public ScrumMaster Leader { get; set; }
+    }
+}
---
+++ ProjectManager/ProjectManager/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectManager.Models
+{
+    public class Project
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Status { get; set; }
+        
+        public List<string> Tasks { get; set; }
+    }
+}
